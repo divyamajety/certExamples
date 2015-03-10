d92573bc6458c3226c2dbb925ddbfa2c9aa6b080
divyamajety <divyamajeti@gmail.com>
2015-03-10 13:36:45 -0400

    First Git Clone through Egit plugin from C:\utils\work\mywork

--- Git Demo/src/com/divya/Demo.java
+++ Git Demo/src/com/divya/Demo.java
@@ -4,6 +4,7 @@ public class Demo {
 	
 	public static void main(String[] srgs){
 		System.out.println("Hello World Git");
+		System.out.println("My First Git Clone through Egit plugin from C:\\utils\\work\\mywork");
 	}
 
 }
