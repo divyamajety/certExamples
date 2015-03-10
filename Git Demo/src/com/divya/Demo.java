86367a11a32e22322d2a29f7acd16118ac2240ab
divyamajety <divya.majeti@gmail.com>
2015-03-10 14:11:51 -0400

    changes made to println

--- Git Demo/src/com/divya/Demo.java
+++ Git Demo/src/com/divya/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 	
 	public static void main(String[] srgs){
 		//This is to demo Egit plugin in eclipse
-		System.out.println("Hello World Git");
+		System.out.println("Hello World Git" + "changes made in github");
 		System.out.println("My First Git Clone through Egit plugin from C:\\utils\\work\\mywork");
 	}
 
