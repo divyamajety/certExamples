06c81eae1a995bf6932a25f86b135862d8870769
divyamajety <divya.majeti@gmail.com>
2015-03-10 13:50:27 -0400

    println line changed in github

--- Git Demo/src/com/divya/Demo1.java
+++ Git Demo/src/com/divya/Demo1.java
@@ -3,7 +3,7 @@ package com.divya;
 public class Demo1 {
 	
 	public static void main(String[] srgs){
-		System.out.println("1. Demo1 added from github"+"2. changed from local ");
+		System.out.println("1. Demo1 added from github"+"2. changed from local" + "3. Want to check for merge conflicts");
 	}
 
 }
