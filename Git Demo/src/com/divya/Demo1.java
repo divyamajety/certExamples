e3332d17b754a781a28a5bb240045ad131e3bc84
divyamajety <divyamajeti@gmail.com>
2015-03-10 13:48:22 -0400

    Demo1 changed from local repo

--- Git Demo/src/com/divya/Demo1.java
+++ Git Demo/src/com/divya/Demo1.java
@@ -3,7 +3,7 @@ package com.divya;
 public class Demo1 {
 	
 	public static void main(String[] srgs){
-		System.out.println("Demo1 added from github");
+		System.out.println("1. Demo1 added from github"+"2. changed from local ");
 	}
 
 }
