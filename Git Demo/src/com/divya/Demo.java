dca88821988f74a5723d2b42b4949fc260a3b6c6
divyamajety <divyamajeti@gmail.com>
2015-03-10 14:08:03 -0400

    made changes to Demo

--- Git Demo/src/com/divya/Demo.java
+++ Git Demo/src/com/divya/Demo.java
@@ -3,6 +3,7 @@ package com.divya;
 public class Demo {
 	
 	public static void main(String[] srgs){
+		
 		System.out.println("Hello World Git");
 		System.out.println("My First Git Clone through Egit plugin from C:\\utils\\work\\mywork");
 	}
